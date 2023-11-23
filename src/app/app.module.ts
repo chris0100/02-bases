5f7ff77b9771b8dfa3937afc22c67d5f3b456aa4
Christian Rodriguez <alexroguez59@gmail.com>
2023-11-22 21:03:03 -0500

    Implement operations to deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {CounterModule} from "./counter/components/counter.module";
 import {HeroesModule} from "./heroes/heroes.module";
+import {DbzModule} from "./dbz/dbz.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent
+    ],
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
